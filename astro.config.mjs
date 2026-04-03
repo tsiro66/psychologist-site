40818e4f8290a26cf0509b4bbf61a40498cebb17
Thodoris Tsironis <theogr99@gmail.com>
2026-04-03 22:23:51 +0300

    Installed vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,33 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, fontProviders } from "astro/config";
+
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-      vite: {
+  vite: {
     plugins: [tailwindcss()],
   },
-   fonts: [
-      {
-          provider: fontProviders.google(),
-          name: "Google Sans",
-          cssVariable: "--font-google",
-          fallbacks:["sans-serif"],
-          subsets: ["greek", "latin"],
-          styles: ["normal"],
-        },
-        {
-          provider: fontProviders.google(),
-          name: "Source Serif 4",
-          cssVariable: "--font-noto",
-          fallbacks:["sans-serif"],
-          subsets: ["greek", "latin"],
-          styles: ["normal"],
-        },
+
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Google Sans",
+      cssVariable: "--font-google",
+      fallbacks: ["sans-serif"],
+      subsets: ["greek", "latin"],
+      styles: ["normal"],
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Source Serif 4",
+      cssVariable: "--font-noto",
+      fallbacks: ["sans-serif"],
+      subsets: ["greek", "latin"],
+      styles: ["normal"],
+    },
   ],
+
+  adapter: vercel(),
 });
