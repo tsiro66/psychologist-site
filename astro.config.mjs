9c49dd687cf2b3864afc2457b9b3e6e9d1d9b50a
Thodoris Tsironis <theogr99@gmail.com>
2026-04-03 21:27:12 +0300

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
       vite: {
     plugins: [tailwindcss()],
   },
+   fonts: [
+      {
+          provider: fontProviders.google(),
+          name: "Google Sans",
+          cssVariable: "--font-google",
+          fallbacks:["sans-serif"],
+          subsets: ["greek", "latin"],
+          styles: ["normal"],
+        },
+        {
+          provider: fontProviders.google(),
+          name: "Source Serif 4",
+          cssVariable: "--font-noto",
+          fallbacks:["sans-serif"],
+          subsets: ["greek", "latin"],
+          styles: ["normal"],
+        },
+  ],
 });
